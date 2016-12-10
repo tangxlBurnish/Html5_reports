89575efd9d60a74c8a011f5041cf536462ada283
WuYifanX <510182912@qq.com>
2016-12-10 15:22:32 +0800

    第一阶段完成
    完成了首页及其首页的重构

---
+++ js/test-H5.js
@@ -0,0 +1,92 @@
+/**
+ * Created by wuyifan on 16/11/23.
+ */
+
+/**
+ * 返回一个H5对象
+ * @returns {H5} H5对象,拥有addPage,addComponent的方法;
+ * @constructor
+ */
+
+var H5 = function(){
+    //给这对象一个ID用于辨识
+    this.id = ("h5_" + Math.random()).replace(".","_");
+
+    //后面有一个hide的原因是可能中途加载不完全就显示了, 因此等所有的元素先hide,然后等都加载完成再显示(使用提供的loader方法)
+    this.el = $("<div class='h5' id = '" +this.id +"'>").hide();
+    $("body").append(this.el);
+
+    //每次新增的page丢在这个数组里面, 这是为了实现链式调用的中间缓冲栈,每次在addPage就丢进去,然后addComponent就拿出来;
+    this.page = [];
+
+    /**
+     * 新增一个页面
+     * @param {string} name 组建的名称,会加入到ClassName中
+     * @param text {string} 页内的默认文本
+     * @returns {H5} H5对象,可以重复适用H5对象支持的方法
+     * @future: 这个函数未来的重构中要加入H5的原型中;
+     */
+    this.addPage = function(name, text){
+        //@improve:这里我在思考这里的page需不需要加上this;我觉得不加,就是一个闭包,并不会产生一个变量;但是可能有性能的问题;
+
+        //这里加上section是为了配合fullPage来产生一个可以滑动的页面;
+        var page = $("<div class = 'h5_page section'>");
+        if( name != undefined){
+            page.addClass("h5_page_"+ name)
+        }
+        if( text != undefined){
+            page.text(text);
+        }
+        this.page.push(page);
+        this.el.append(page);
+        return this;
+    };
+
+    /**
+     * 新增一个组件
+     * @param name {string} 组件的名称
+     * @param outCfg   {object} 组件的配置文件
+     * @returns {H5}
+     */
+    this.addComponent = function(name, outCfg){
+        var cfg = outCfg || {},
+            component,
+        //因为先执行了addPage,然后从addPage的后面链式调用addComponent;因此需要从缓冲栈中获取第一个page
+        page = this.page.slice(-1)[0];
+
+        //如果cfg里面没有type属性,那么会在后面加入type属性
+        cfg = $.extend({
+            type:"base"
+        },cfg);
+
+        //根据添加的属性type来改变组件的cfg特性
+        switch(cfg.type){
+            case "base":
+                component = new H5ComponentBase(name,cfg);
+                break;
+            default:
+                break;
+        }
+        page.append(component);
+        return this;
+    };
+
+    /**
+     * H5对象初始化呈现
+     */
+    this.loader = function(){
+        this.el.fullpage({
+            onLeave: function( index, nextIndex, direction){
+                $(this).find(".h5_component").trigger("onLeave");
+            },
+            afterLoad:function( anchorLink, index){
+                $(this).find(".h5_component").trigger("onLoad");
+            }
+        });
+        this.el.show();
+    };
+
+    return this;
+};
+
+
---
+++ js/test-H5ComponentBase.js
@@ -0,0 +1,64 @@
+/**
+ * Created by wuyifan on 16/11/21.
+ */
+//基本图文组件对象
+
+var H5ComponentBase = function( name, outerCfg ){
+    //测试,如果传入的outerCfg为空,就赋值使用空对象;
+    var cfg = outerCfg||{};
+
+    //加入一个随机Id,未来可能有用;
+    var id = ( "h5_c_" + Math.random()).replace(".", "_");
+    //因为作为一个base组件,需要知道是什么类型的组件,因此赋值className(class);
+    var cls = "h5_component_" + cfg.type +" h5_component_name_" + name;
+
+    //这里有很多class的组件,比如说h5_component,h5_component_base,h5_component_name
+    //h5_component 这是表示一个组件,可以通过 $(this).find(".h5_component") 找到所有组件
+    //h5_component_base 表示这是某个类型的图文组件 base, pie, point 等表示某些样式的附加
+    //h5_component_name_myName 是为了pinpoint某一个组件来指定css
+    var component = $( "<div class = 'h5_component " + cls + "' id='" +id+"'>" );
+    
+    //&&相当于,如果我前面的一项成立( e,g:cfg有text属性),那么我就执行后面的操作;
+    cfg.text   && component.text(cfg.text);
+    cfg.width  && component.width(cfg.width);
+    cfg.height && component.height(cfg.height);
+    cfg.css  && component.css( cfg.css );
+    cfg.bg   && component.css("backgroundImage", "url(" +cfg.bg+")");
+    
+    if( cfg.center === true){
+        component.css({
+            marginLeft : ( cfg.width/2 * -1) +"px",
+            left : "50%"
+        });
+    }
+    
+    //这里给 component 绑定两个事件,但是 base ,所以无法知晓绑定之后要做什么,所以先绑定之后就标注下class
+    component.on("onLoad",function(){
+
+        setTimeout(function(){
+            $(this).addClass(cls + "_load").removeClass(cls +"_leave");
+            //animate 是Jquery的动画方法,接受参数为最后的显示结果.
+            cfg.animateIn && component.animate(cfg.animateIn);
+        },cfg.delay||0);
+
+        return false;
+    });
+    
+    component.on("onLeave",function(){
+        $(this).addClass(cls + "_leave").removeClass(cls +"_load");
+        cfg.animateOut && component.animate(cfg.animateOut);
+        return false;
+    });
+
+    var leave = true;
+    $("body").click(function(){
+        if (leave){
+            component.trigger("onLoad");
+        }else{
+            component.trigger("onLeave");
+        }
+        leave = !leave;
+    });
+
+    return component;
+};
